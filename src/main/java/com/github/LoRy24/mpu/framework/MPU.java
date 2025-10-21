46534a5b590835b171b40ebc4e7efaec8c770556
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2025-10-21 23:41:50 +0200

    Complete the main class JavaDoc.

--- src/main/java/com/github/LoRy24/mpu/framework/MPU.java
+++ src/main/java/com/github/LoRy24/mpu/framework/MPU.java
@@ -12,14 +12,51 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+/**
+ * The {@link MPU} class is the main utility class to interact with any Minecraft server that is reachable from your
+ * plugin's host. It contains utility to check the online status of a server, and to get a Server List ping, formatted
+ * in the {@link ServerListPingResult} object.
+ * <p>
+ * Also, in some scenarios, you might need to set a custom timeout value: with the utilities offered by this class, you
+ * are allowed to do it.
+ *
+ * @version 1.0.0
+ * @author LoRy24
+ *
+ * @see ServerListPingResult
+ * @see HandshakePacket
+ * @see StatusRequestPacket
+ * @see StatusResponsePacket
+ * @see com.github.LoRy24.mpu.framework.packets.AbstractPacket
+ */
 @SuppressWarnings("unused")
 @UtilityClass
 public class MPU {
 
+    /**
+     * This function checks if a server is online, so it performs a base socket check setting as default timeout 5000 ms.
+     * Uses the {@code isServerOnline(String host, int port, int timeout)} function to achieve the result.
+     *
+     * @param host The server's host.
+     * @param port The server's port.
+     *
+     * @return If the server is online. No exceptions are thrown.
+     */
     public boolean isServerOnline(String host, int port) {
         return isServerOnline(host, port, 5000);
     }
 
+    /**
+     * This function, does the same thing as the {@code isServerOnline(String host, int port)} function, but it does that
+     * primarily. In fact, this is the function that runs the code to check if the server is online, but it provides the
+     * possibility to specify a timeout value.
+     *
+     * @param host The server's host.
+     * @param port The server's port.
+     * @param timeout The socket connection timeout value.
+     *
+     * @return If the server is online.
+     */
     public boolean isServerOnline(String host, int port, int timeout) {
         try {
             Socket socket = new Socket();
@@ -32,10 +69,41 @@ public class MPU {
         }
     }
 
+    /**
+     * This function performs a ServerList Ping action (see the
+     * <a href="https://minecraft.wiki/w/Java_Edition_protocol/Server_List_Ping">Minecraft Protocol Reference</a> for more
+     * details) and returns the ping result.
+     * <p>
+     * The only thing that this function does not return is the server's ping. It will be implemented soon in a new and
+     * specific function. It uses the {@code getServerListPingResult(String host, int port, int protocolVersion, int timeout)}
+     * function to achieve its result and sets a default timeout value of 5000 ms.
+     *
+     * @param host The server's host.
+     * @param port The server's port.
+     * @param protocolVersion The server's protocol version or one used to catch the version issue.
+     *
+     * @return The result of the ping, parsed in the {@link ServerListPingResult} object.
+     *
+     * @throws IOException If the connection fails to read the data or to connect.
+     */
     public ServerListPingResult getServerListPingResult(String host, int port, int protocolVersion) throws IOException {
         return getServerListPingResult(host, port, protocolVersion, 5000);
     }
 
+    /**
+     * This function does the same things as the {@code getServerListPingResult(String host, int port, int protocolVersion)}
+     * one, because this is the one called from it. The difference is that with this function you will be able to specify
+     * a custom timeout value.
+     *
+     * @param host The server's host.
+     * @param port The server's port.
+     * @param protocolVersion The server's protocol version or one used to catch the version issue.
+     * @param timeout The custom timeout value.
+     *
+     * @return The result of the ping, parsed in the {@link ServerListPingResult} object.
+     *
+     * @throws IOException If the connection fails to read the data or to connect.
+     */
     public ServerListPingResult getServerListPingResult(String host, int port, int protocolVersion, int timeout) throws IOException {
         // Create the socket
         Socket socket = new Socket();
